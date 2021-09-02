4bb55beb77cc60d7048214daa7f6336287b5d58c
JanOPedersen <jan.o.pedersen@mail.dk>
2021-09-02 09:56:24 +0200

    Add files via upload

---
+++ crop_rotated_square.py
@@ -0,0 +1,35 @@
+"""Given an image and a set of 4 points representing a square
+   we crop the image by that square so that a new cropped image
+   is returned"""
+import cv2
+import numpy as np
+
+def crop_rotated_square(img, pts):
+    """The inputs to this function are the corner points of the square
+       However this will also work for rectangles """
+    rect = cv2.minAreaRect(pts)
+
+    # the order of the box points: bottom left, top left, top right,
+    # bottom right
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+
+    # get width and height of the detected rectangle
+    width = int(rect[1][0])
+    height = int(rect[1][1])
+
+    src_pts = box.astype("float32")
+    # coordinate of the points in box points after the rectangle has been
+    # straightened
+    dst_pts = np.array([[0, height-1],
+                        [0, 0],
+                        [width-1, 0],
+                        [width-1, height-1]], dtype="float32")
+
+    # the perspective transformation matrix
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+
+    # directly warp the rotated rectangle to get the straightened rectangle
+    warped = cv2.warpPerspective(img, M, (width, height))
+
+    return warped
